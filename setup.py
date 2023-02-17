5867d203fc8f7ffcaad505b48dbf3ed0f8ce5415
Bombshell2 <62397402+Bombshell2@users.noreply.github.com>
2023-02-17 11:31:00 -0500

    clean

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-if sys.version_info[0] < 3 or sys.version_info[1] < 4:
-    raise Exception("Must be using Python Version <= 3.4, please update at https://www.python.org/downloads/")
-print("Installing discord.py...")
-os.system("pip install discord")
-print("Installing requests...")
-os.system("pip install requests")
-print("Done!")
